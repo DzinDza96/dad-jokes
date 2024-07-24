d85ea8355262e7cfdb82eb824a3620aaeba36831
DzinDza96 <dzindza.mladji@gmail.com>
2024-07-24 15:51:39 +0200

    adapt schema and .yml

--- schema.sql
+++ schema.sql
@@ -1,18 +1,21 @@
--- Create the database
-CREATE DATABASE jokesdb;
+IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = 'jokesdb')
+BEGIN
+    CREATE DATABASE jokesdb;
+END
 
--- Switch to the new database
 USE jokesdb;
 
--- Create the jokes table
-CREATE TABLE jokes (
-    id INT PRIMARY KEY IDENTITY(1,1),
-    question NVARCHAR(255),
-    answer NVARCHAR(255),
-    rating INT
-);
+IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='jokes' AND xtype='U')
+BEGIN
+    CREATE TABLE jokes (
+        id INT PRIMARY KEY IDENTITY(1,1),
+        question NVARCHAR(255),
+        answer NVARCHAR(255),
+        rating INT
+    );
+END
 
--- Insert some dummy data
+-- Insert dummy data
 INSERT INTO jokes (question, answer, rating) VALUES ('What do you call cheese that isn''t yours?', 'Nacho cheese', 5);
 INSERT INTO jokes (question, answer, rating) VALUES ('Why don''t skeletons fight each other?', 'They don''t have the guts.', 4);
 INSERT INTO jokes (question, answer, rating) VALUES ('What do you call fake spaghetti?', 'An impasta.', 3);
